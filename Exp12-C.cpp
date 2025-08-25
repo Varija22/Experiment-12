24add9818068ae1714b8c874bcc6180c89b87f54
Varija22 <varija.korti.btech2024@sitpune.edu.in>
2025-08-25 15:14:05 +0530

    Add files via upload

---
+++ Exp12-C.cpp
@@ -0,0 +1,23 @@
+//Varija K
+//24070123165
+#include <iostream>
+#include <iostream>
+using namespace std;
+
+class DefaultConst {
+    int x, y;
+public:
+    DefaultConst() {   
+        x = 5;
+        y = 10;
+    }
+    void show() {
+        cout << "x=" << x << " y=" << y << endl;
+    }
+};
+
+int main() {
+    DefaultConst d;
+    d.show();
+
+}
